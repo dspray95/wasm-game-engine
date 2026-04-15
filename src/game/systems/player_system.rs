3263edc40ce47d8faed7602de950637dc19ac49d
unknown <dspray95@gmail.com>
2026-04-15 22:15:29 +0100

    tidyup

--- src/game/systems/player_system.rs
+++ src/game/systems/player_system.rs
@@ -5,13 +5,13 @@ use crate::{
     engine::{
         camera::camera::Camera,
         ecs::{
-            components::{ transform::Transform, velocity::{ self, Velocity } },
+            components::velocity::Velocity,
             resources::input_state::InputState,
             system::SystemContext,
             world::World,
         },
     },
-    game::components::player::{ self, Player },
+    game::components::player::{ Player },
 };
 
 const Z_MOVEMENT_SPEED: f32 = 10.0;
@@ -20,7 +20,7 @@ const STRAFE_SPEED: f32 = 4.0;
 pub fn player_system(world: &mut World, _system_context: &mut SystemContext) {
     let input = world.get_resource::<InputState>().unwrap().clone();
 
-    if let Some((velocity, _player)) = world.query_iter::<(&mut Velocity, &Player)>().next() {
+    if let Some((_player, velocity)) = world.query_iter::<(&Player, &mut Velocity)>().next() {
         // Move forward
         velocity.z = Z_MOVEMENT_SPEED;
 
@@ -35,6 +35,8 @@ pub fn player_system(world: &mut World, _system_context: &mut SystemContext) {
 
     if let Some(camera) = world.get_resource_mut::<Camera>() {
         // Also move the camera forward
+        // We have to use delta_time here, since the camera isn't actually a component on an entity
+        // And the engine's velocity system won't move it for us - TODO
         let (yaw_sin, yaw_cos) = camera.yaw.0.sin_cos();
         let forward = Vector3::new(yaw_cos, 0.0, yaw_sin).normalize();
         camera.position += forward * Z_MOVEMENT_SPEED * _system_context.delta_time;
